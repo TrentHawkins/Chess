a2a7aeddbf75b4c2da82420d1cb09dc0efd7992a
Vassilis Palassopoulos <palasso@gmail.com>
2022-08-27 15:59:30 +0300

    Add unit tests to the project

---
+++ test_chess.py
@@ -0,0 +1,24 @@
+"""Unit tests for the Chess project."""
+
+initial_board_state = """
+▐\x1b[7m  A B C D E F G H  \x1b[0m▌
+▐\x1b[7m8\x1b[0m▌♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜▐\x1b[7m8\x1b[0m▌
+▐\x1b[7m7\x1b[0m▌♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟▐\x1b[7m7\x1b[0m▌
+▐\x1b[7m6\x1b[0m▌               ▐\x1b[7m6\x1b[0m▌
+▐\x1b[7m5\x1b[0m▌               ▐\x1b[7m5\x1b[0m▌
+▐\x1b[7m4\x1b[0m▌               ▐\x1b[7m4\x1b[0m▌
+▐\x1b[7m3\x1b[0m▌               ▐\x1b[7m3\x1b[0m▌
+▐\x1b[7m2\x1b[0m▌♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙▐\x1b[7m2\x1b[0m▌
+▐\x1b[7m1\x1b[0m▌♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖▐\x1b[7m1\x1b[0m▌
+▐\x1b[7m  A B C D E F G H  \x1b[0m▌
+
+"""
+
+
+class TestBoard:
+    """Unit tests for the board."""
+
+    def test_board(self):
+        """Test initial state of the board."""
+        from src.board import Board
+        assert str(Board()) == initial_board_state
