c745cb2d17af4efa3fce0bf293d35c61d0398d5e
Trent Hawkins <spcom1024@gmail.com>
2022-08-28 14:19:23 +0300

    Implement test for piece referencing on the board

--- test_chess.py
+++ test_chess.py
@@ -1,5 +1,6 @@
 """Unit tests for the Chess project."""
 
+
 initial_board_state = """
 ▐\033[7m  A B C D E F G H  \033[0m▌
 ▐\033[7m8\033[27m\033[4m▌♜│♞│♝│♛│♚│♝│♞│♜▐\033[24m\033[7m8\033[0m▌
@@ -21,4 +22,16 @@ class TestBoard:
     def test_board(self):
         """Test initial state of the board."""
         from src.board import Board
+
         assert str(Board()) == initial_board_state
+
+    def test_square(self):
+        """Test piece referencing on the board."""
+        from src.board import Board
+
+        board = Board()
+
+        for rank in board._board:
+            for piece in rank:
+                if piece is not None:
+                    assert board[board.square_of(piece)] is piece
