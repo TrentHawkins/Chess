cb2262b0df4c948b85d5eb96824048a261a37d6f
Trent Hawkins <spcom1024@gmail.com>
2022-08-28 13:00:05 +0300

    Update unit test with the new representation of the board

--- test_chess.py
+++ test_chess.py
@@ -2,14 +2,14 @@
 
 initial_board_state = """
 ▐\x1b[7m  A B C D E F G H  \x1b[0m▌
-▐\x1b[7m8\x1b[0m▌♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜▐\x1b[7m8\x1b[0m▌
-▐\x1b[7m7\x1b[0m▌♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟▐\x1b[7m7\x1b[0m▌
-▐\x1b[7m6\x1b[0m▌               ▐\x1b[7m6\x1b[0m▌
-▐\x1b[7m5\x1b[0m▌               ▐\x1b[7m5\x1b[0m▌
-▐\x1b[7m4\x1b[0m▌               ▐\x1b[7m4\x1b[0m▌
-▐\x1b[7m3\x1b[0m▌               ▐\x1b[7m3\x1b[0m▌
-▐\x1b[7m2\x1b[0m▌♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙▐\x1b[7m2\x1b[0m▌
-▐\x1b[7m1\x1b[0m▌♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖▐\x1b[7m1\x1b[0m▌
+▐\x1b[7m8\x1b[27m\x1b[4m▌♜│♞│♝│♛│♚│♝│♞│♜▐\x1b[24m\x1b[7m8\x1b[0m▌
+▐\x1b[7m7\x1b[27m\x1b[4m▌♟│♟│♟│♟│♟│♟│♟│♟▐\x1b[24m\x1b[7m7\x1b[0m▌
+▐\x1b[7m6\x1b[27m\x1b[4m▌ │ │ │ │ │ │ │ ▐\x1b[24m\x1b[7m6\x1b[0m▌
+▐\x1b[7m5\x1b[27m\x1b[4m▌ │ │ │ │ │ │ │ ▐\x1b[24m\x1b[7m5\x1b[0m▌
+▐\x1b[7m4\x1b[27m\x1b[4m▌ │ │ │ │ │ │ │ ▐\x1b[24m\x1b[7m4\x1b[0m▌
+▐\x1b[7m3\x1b[27m\x1b[4m▌ │ │ │ │ │ │ │ ▐\x1b[24m\x1b[7m3\x1b[0m▌
+▐\x1b[7m2\x1b[27m\x1b[4m▌♙│♙│♙│♙│♙│♙│♙│♙▐\x1b[24m\x1b[7m2\x1b[0m▌
+▐\x1b[7m1\x1b[27m\x1b[4m▌♖│♘│♗│♕│♔│♗│♘│♖▐\x1b[24m\x1b[7m1\x1b[0m▌
 ▐\x1b[7m  A B C D E F G H  \x1b[0m▌
 
 """
